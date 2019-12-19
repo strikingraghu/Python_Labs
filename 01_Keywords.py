4fcf05a2d185fc88e97a5b9e425bfe4d471f4be0
Raghavendra Guttur <strikingraghu@gmail.com>
2019-12-19 11:11:33 +0530

    Python

--- 01_Keywords.py
+++ 01_Keywords.py
@@ -4,6 +4,7 @@ They are used to define the syntax and structure of the Python language.
 In Python, keywords are case sensitive.
 There are 33 keywords in Python 3.7. This number can vary slightly in the course of time."""
 
+import keyword
 
 # Python code to demonstrate keywords
 print("# Python code to demonstrate keywords")
@@ -111,3 +112,49 @@ def outer_two():
 
 outer_two()
 print()
+
+# Python code to to check if a string is a valid keyword
+print("Python code to to check if a string is a valid keyword")
+
+s1 = "geeks"
+s2 = "for"
+s3 = "else"
+s4 = "elif"
+s5 = "break"
+s6 = "try"
+s7 = "assert"
+
+if keyword.iskeyword(s1):
+    print("S1 is a keyword - ", s1)
+else:
+    print("S1 is not a keyword -", s1)
+
+if keyword.iskeyword(s2):
+    print("S2 is a keyword - ", s2)
+else:
+    print("S2 is not a keyword -", s2)
+
+if keyword.iskeyword(s3):
+    print("S3 is a keyword - ", s3)
+else:
+    print("S3 is not a keyword -", s3)
+
+if keyword.iskeyword(s4):
+    print("S4 is a keyword - ", s4)
+else:
+    print("S4 is not a keyword -", s4)
+
+if keyword.iskeyword(s5):
+    print("S5 is a keyword - ", s5)
+else:
+    print("S5 is not a keyword -", s5)
+
+if keyword.iskeyword(s6):
+    print("S6 is a keyword - ", s6)
+else:
+    print("S6 is not a keyword -", s6)
+
+if keyword.iskeyword(s7):
+    print("S7 is a keyword - ", s7)
+else:
+    print("S7 is not a keyword -", s7)
