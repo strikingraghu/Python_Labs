66ea825f4ba89b087ebf113e3321f2cf5ae3a61e
Raghavendra Guttur <strikingraghu@gmail.com>
2019-12-20 20:44:44 +0530

    Python

--- 03_Variables.py
+++ 03_Variables.py
@@ -49,3 +49,19 @@ print("# Strings in Python code")
 string_1 = "Hello, you are in Bangalore"
 string_2 = "Indeed, its a great city"
 print("Length - string_1 =", len(string_1))
+print("Display String's 4 Character =", string_1[4])
+print("Display String's between 2 & 4 =", string_2[2:4])
+print("Display String's =", string_2[:4])
+print("Display String's =", string_2[2:])
+print("Display String's =", string_2[:])
+
+# Lists
+print("# Lists in Python code")
+list_1 = [22, "Ram", 339273.223, 3778.99271j, "Suresh"]
+print("Index value of String =", list_1.index("Suresh"))
+list_1.append("Apple")
+print("List values after appending an element =", list_1)
+list_2 = list_1.copy()
+print("List_2 values after copying it via List_1 =", list_2)
+list_2.reverse()
+print("Reversing List_1 in this context =", list_2)
