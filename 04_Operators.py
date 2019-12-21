f47bcdd545aecfef4442437246e5f510e2912c1f
Raghavendra Guttur <strikingraghu@gmail.com>
2019-12-21 18:21:46 +0530

    Python

---
+++ 04_Operators.py
@@ -0,0 +1,27 @@
+"""
+    Operators are the constructs which can manipulate the value of operands.
+    Consider the expression 4 + 5 = 9. Here, 4 and 5 are called operands and + is called operator.
+    Types of Operator:
+        Python language supports the following types of operators.
+            Arithmetic Operators
+            Comparison (Relational) Operators
+            Assignment Operators
+            Bitwise Operators
+            Membership Operators
+            Identity Operators
+            Operators Precedence
+"""
+
+# Example - Python Arithmetic Operators
+# Assume variable A holds 10 and variable B holds 20, then −
+
+# Python code for Arithmetic operations
+print("# Python code for Arithmetic operations")
+var_a = 10
+var_b = 20
+print("Addition =", var_a + var_b)
+print("Subtraction =", var_b - var_a)
+print("Multiplication =", var_a * var_b)
+print()
+
+
